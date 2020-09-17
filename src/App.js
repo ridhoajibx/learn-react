082aa8e71eb2c7a6535430316b3a31154fa1da0b
ridhoajibx <ridhoajibx@gmail.com>
2020-09-18 02:48:25 +0700

    [ridho]--search data by identifier state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Axios from 'axios'
 export default function App() {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(false)
+    const [identifier, setIdentifier] = useState(1)
 
     const getUsers = async () => {
         setLoading(true)
         try {
             setLoading(false)
-            let response = await Axios.get(`https://jsonplaceholder.typicode.com/users`)
+            let response = await Axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`)
             setUsers(response.data)
         } catch (error) {
             setLoading(true)
@@ -19,12 +20,13 @@ export default function App() {
 
     useEffect(() => {
         getUsers();
-    })
+    }, [identifier])
 
     return (
         <div className="py-4">
             <div className="container">
                 <div className="row">
+                    <input type="text" name="identifier" value={ identifier } onChange={ (e) => setIdentifier(e.target.value) } className="form-control"/>
                     {
                         loading ? <div>Loading ...</div> :
                             <table className="table">
@@ -39,15 +41,13 @@ export default function App() {
                                 </thead>
                                 <tbody>
                                     {
-                                        users.map((user, index) => (
-                                            <tr key={index}>
-                                                <td>{user.name}</td>
-                                                <td>{user.username}</td>
-                                                <td>{user.email}</td>
-                                                <td>{user.phone}</td>
-                                                <td>{user.website}</td>
-                                            </tr>
-                                        ))
+                                        <tr>
+                                            <td>{users.name}</td>
+                                            <td>{users.username}</td>
+                                            <td>{users.email}</td>
+                                            <td>{users.phone}</td>
+                                            <td>{users.website}</td>
+                                        </tr>
                                     }
                                 </tbody>
                             </table>
