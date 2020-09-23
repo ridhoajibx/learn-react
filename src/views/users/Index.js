b595cc64ff460a07384cba74563474b4150a9364
ridhoajibx <ridhoajibx@gmail.com>
2020-09-24 02:27:24 +0700

    [ridho]--create loading state

--- src/views/users/Index.js
+++ src/views/users/Index.js
@@ -4,13 +4,16 @@ import { NavLink } from 'react-router-dom';
 
 function Index() {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const getUsers = async () => {
+        setLoading(true)
         try {
             let response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            
             setUsers(response.data)
+            setLoading(false)
         } catch (e) {
+            setLoading(true)
             console.log(e.message);
         }
     }
@@ -22,22 +25,28 @@ function Index() {
         <div className="container">
             <div className="row">
                 {
-                    users.map((user, index) => {
-                        return (
-                            <div key={index} className="col-md-4">
-                                <div className="card mb-4">
-                                    <div className="card-header">{user.name}</div>
-                                    <div className="card-body">
-                                        <div className="mb-4">
-                                            <a href={`https://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a>
+                    loading ?
+                    <div>Loading ...</div> :
+                    <>
+                    {
+                        users.map((user, index) => {
+                            return (
+                                <div key={index} className="col-md-4">
+                                    <div className="card mb-4">
+                                        <div className="card-header">{user.name}</div>
+                                        <div className="card-body">
+                                            <div className="mb-4">
+                                                <a href={`https://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a>
+                                            </div>
+    
+                                            <NavLink to={`users/${user.id}`} className="btn btn-primary">View propfile</NavLink>
                                         </div>
-
-                                        <NavLink to={`users/${user.id}`} className="btn btn-primary">View propfile</NavLink>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
+                            )
+                        })
+                    }
+                    </>
                 }
             </div>
         </div>
